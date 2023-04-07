8f172082063dc6a8fc26e5969f5ed1865d8096a5
deividramirezarias <deivid.innovatec@gmail.com>
2023-04-07 14:12:46 -0500

    ajustes
    ajustes

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -24,7 +24,7 @@
       .col-lg-12(data-aos="fade-left")     
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)            
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/MoY3UEB8vCk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)            
 
     .row.mt-5
       p Para ampliar información sobre cada una de las temáticas abordadas en el video, se ponen a disposición los siguientes documentos:
